174a9580bc052ce3d91a9b853ad872f521c06f4a
Axel Espinosa <axeldavid45@gmail.com>
2020-07-27 14:27:40 -0500

    strict mode added in missing files

--- utils/middleware/validationData-handler.js
+++ utils/middleware/validationData-handler.js
@@ -1,9 +1,12 @@
+'use strict'
+const boom = require('@hapi/boom');
+
 const validate =  () => false;
 
 const validationHandler = (schema, check = 'body') =>{
   return function handler(req, res, next) {
     const error = validate(req[check], schema);
-    error ? next(new Error(error)) : next();
+    error ? next(boom.badRequest(error)) : next();
   };
 }
 
