5329607760b21f7d033d8ce44dd72b9091a5fbb6
Axel Espinosa <axeldavid45@gmail.com>
2020-11-28 11:51:35 -0600

    refactor: environment variables added

--- config/index.js
+++ config/index.js
@@ -8,6 +8,11 @@ const configs = {
   dbUser: encodeURIComponent(process.env.MONGO_USER),
   dbName: encodeURIComponent(process.env.MONGO_DBNAME),
   dbHost: encodeURIComponent(process.env.MONGO_HOST),
-  dbPassword: encodeURIComponent(process.env.MONGO_PASSWORD)
+  dbPassword: encodeURIComponent(process.env.MONGO_PASSWORD),
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 }
 module.exports = configs
